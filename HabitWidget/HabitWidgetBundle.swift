ebebb8c8431e074f7ed0be27d9ac3708e9b4ddf3
Yusuf Eren Utku <yerenutku@gmail.com>
2024-10-14 13:05:06 +0200

    First working solution implemented. Widget shows a data from habit persistence data.

---
+++ HabitWidget/HabitWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  HabitWidgetBundle.swift
+//  HabitWidget
+//
+//  Created by Utku, YE (Yusuf Eren) on 23/09/2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct HabitWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        HabitWidget()
+    }
+}
