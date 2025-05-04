80798c8a140108d79fd09e6e8b543305a63d10d2
AymanL <40838419+AymanL@users.noreply.github.com>
2025-05-04 12:13:35 +0200

    useless changes

--- Habit/Controllers/DataController.swift
+++ Habit/Controllers/DataController.swift
@@ -45,7 +45,6 @@ class DataController: ObservableObject {
         if inMemory {
             container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
         }
-        
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
